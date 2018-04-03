28d5608b2cb2c53d14c6209343424045a4af5fb7
Harpsichord <tliu1217@163.com>
2018-04-03 22:03:05 +0800

    Set DB Password

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ HOST = "127.0.0.1"
 PORT = "3306"
 DB = "mytodo"
 USER = "root"
-PASS = "123456"  # Harp@1207MySQL
+PASS = "123456"
 CHARSET = "utf8"
 DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}?charset={}".format(USER, PASS, HOST, PORT, DB, CHARSET)
 SQLALCHEMY_DATABASE_URI = DB_URI
